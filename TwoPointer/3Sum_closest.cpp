aecb651b8f6e19cf30aa860ad54e01861d6377a4
Daksh Pant <125189255+dakshpant@users.noreply.github.com>
2026-04-03 13:09:42 +0530

    :

--- TwoPointer/3Sum_closest.cpp
+++ TwoPointer/3Sum_closest.cpp
@@ -1,22 +1,28 @@
-// #include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
 using namespace std;
+
 int threeSumClosest(vector<int>& nums, int target){
     sort(nums.begin(), nums.end());
     int max_diff = INT_MAX;
     int best_sum = 0;
+
     for(int i = 0; i < nums.size() - 2; i++){
-        int left = i+1;
-        int right = nums.size()-1;
+        int left = i + 1;
+        int right = nums.size() - 1;
+
         while(left < right){
-            int sum  = nums[i]+nums[left]+nums[right];
+            int sum = nums[i] + nums[left] + nums[right];
             int diff = abs(sum - target);
+
             if(diff < max_diff) {
                 max_diff = diff;
-                best_sum  = sum;
+                best_sum = sum;
             }
 
-            if(sum == target) return sum;nano ~/.bashrc
+            if(sum == target) return sum;
             else if(sum < target) left++;
             else right--;
         }
@@ -25,10 +31,11 @@ int threeSumClosest(vector<int>& nums, int target){
 }
 
 int main(){
-     vector<int> nums = {-1, 2, 1, -4};
+    vector<int> nums = {-1, 2, 1, -4};
     int target = 1;
 
-  int result = threeSunClosest(nums, target)
-  cout << result << endl;
+    int result = threeSumClosest(nums, target);
+    cout << result << endl;
+
     return 0;
 }
