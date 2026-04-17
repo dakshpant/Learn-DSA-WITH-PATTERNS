ba9b040ecf1398de0f959c2e188233c5b89a5936
Daksh Pant <dakshpant15@gmail.com>
2026-04-17 10:38:58 +0530

    Longest Substring without duplicates M1 and M2

--- SlidingWindow/Longest_Substring_Without_Repeating_Characters.cpp
+++ SlidingWindow/Longest_Substring_Without_Repeating_Characters.cpp
@@ -3,6 +3,8 @@
 #include<string>
 #include<climits>
 using namespace std;
+//General approach using while loop and checking for each duplicate 
+/*
 int lengthOfLongestSubstring(string s){
 int low = 0;
 int high = 0;
@@ -22,6 +24,26 @@ for(high = 0; high < s.size(); high++){
     res=max(res, len);
 }
 return (res==INT_MIN?0:res);
+}
+*/
+//bit ooptimized approach 
+int lengthOfLongestSubstring(string s){
+    int low = 0;
+    int high = 0;
+    int res = INT_MIN;
+    unordered_map<char, int> mpp;
+    for(high = 0; high < s.size(); high++){
+        mpp[s[high]]++;//expanding the window by adding elements
+        /*
+        this while loop checha for each high against ite frequency inside the h_map if > 1 decrease one freq and increase te low pointer*/
+        while(mpp[s[high]]>1){
+            mpp[s[low]]--;
+            low++;
+        }
+        int len = high-low+1;
+        res = max(res,len);
+    }
+    return (res == INT_MIN ? 0 : res);
 }
 int main(){
     int ans = lengthOfLongestSubstring("abcabcbb");
