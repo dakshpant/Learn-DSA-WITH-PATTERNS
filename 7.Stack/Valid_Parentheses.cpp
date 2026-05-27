002a9fa02160c2c32bcd335dabc50ce79acae205
Daksh Pant <125189255+dakshpant@users.noreply.github.com>
2026-05-27 23:20:27 +0530

    20.Valid Parentheses

--- 7.Stack/Valid_Parentheses.cpp
+++ 7.Stack/Valid_Parentheses.cpp
@@ -2,6 +2,7 @@
 #include<stack>
 #include<string>
 using namespace std;
+//Mathod 1 Mu way but lefts one edge case of " {]}" where ] is lef behind edge case
 bool validetParentheses(string s){
     stack<char> st;
     for (int i = 0; i < s.size(); i++)
@@ -22,9 +23,34 @@ bool validetParentheses(string s){
     
     return true;
 }
+bool isValid(string s2) {
+        stack<char> st;
+        for (int i = 0; i < s2.size(); i++) {
+            if (s2[i] == '(' || s2[i] == '{' || s2[i] == '[') {
+                st.push(s2[i]);
+            } else {
+                if (st.empty())
+                    return false;
+                if (s2[i] == ')' && st.top() == '(' ||
+                    s2[i] == '}' && st.top() == '{' ||
+                    s2[i] == ']' && st.top() == '[') {
+                    st.pop();
+                }else{
+                    return false;//mis match condition  "(])" => ] => fasle
+                }
+            }
+        }
+        if (!st.empty())
+            return false;
+        return true;
+    }
+
 int main(){
     string s = "([)]";
-    bool ans = validetParentheses(s);
-    cout<< ans << endl;
+    string s2 = "{]}";
+    bool ans1 = validetParentheses(s);
+    bool ans2 = isValid(s2);
+    cout<< ans1 << endl;
+    cout<< ans2 << endl;
     return 0;
 }
