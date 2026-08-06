8293c80ff96ce7df1790b2d8fa3245a86592684f
Daksh Pant <125189255+dakshpant@users.noreply.github.com>
2026-08-06 13:27:03 +0530

    LC:121. Best Time to Buy and Sell Stock

---
+++ 15.DP/Buy_and_sell_stocks_1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int func(vector<int> &prices, int n, int i, int k, vector<vector<int>> &dp){
+    if(i==n) return 0;
+    if(k == 0) return 0;
+    if(dp[i][k] != -1) return dp[i][k];
+
+    if(k == 2)//buy case
+    {
+        int bc1 = func(prices, n, i+1, k-1,dp) - prices[i];
+        int bc2 = func(prices, n, i+1, k, dp);
+        return dp[i][k] = max(bc1, bc2);
+    }
+    else//sell case
+    {
+        int sc1 = func(prices,n,i+1, k-1,dp) + prices[i];
+        int sc2 = func(prices, n, i+1, k,dp);
+        return dp[i][k] = max(sc1, sc2);
+    }
+}
+int buyAndSellStocks(vector<int> &prices) {
+    int n = prices.size();
+    int k = 2;
+    vector<vector<int>> dp(n+1,vector<int>(k+1,-1));
+    return func(prices, n, 0, k, dp);
+}
+int main()
+{
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << buyAndSellStocks(prices);
+    return 0;
+}
