35322f4c09b16dcbe2d0909e096fdcfaad931347
Daksh Pant <125189255+dakshpant@users.noreply.github.com>
2026-09-03 20:04:00 +0530

    LC:1827. Minimum Operations to Make the Array Increasing

---
+++ 17.GreedyPattern/tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+int minOperations(vector<int> &nums) {
+  int n = nums.size();
+  int count = 0;
+  for (int i = 0; i < n - 1; i++) {
+    if (nums[i] >= nums[i + 1]) {
+      int diff = nums[i] - nums[i + 1];
+      count += diff + 1;
+      nums[i + 1] += diff + 1;
+    }
+  }
+  return count;
+}
+int main() {
+  vector<int> nums = {1, 1, 1};
+  cout << minOperations(nums) << endl;
+  return 0;
+}
